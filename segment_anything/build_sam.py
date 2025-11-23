48a9b15eea48aaa58b2e43c8fa9d25ab1cb2f222
LIMAMMohamedlimam <lmz.mohamedlimam@gmail.com>
2025-11-23 14:31:24 +0100

    fixing state dict loading

--- segment_anything/build_sam.py
+++ segment_anything/build_sam.py
@@ -10,6 +10,7 @@ from functools import partial
 
 from .modeling import ImageEncoderViT, MaskDecoder, PromptEncoder, Sam, TwoWayTransformer
 
+from torch.nn import functional as F
 
 def build_sam_vit_h(checkpoint=None):
     return _build_sam(
@@ -57,6 +58,7 @@ def _build_sam(
     encoder_depth,
     encoder_num_heads,
     encoder_global_attn_indexes,
+    transformer_block,
     checkpoint=None,
 ):
     prompt_embed_dim = 256
@@ -77,6 +79,7 @@ def _build_sam(
             global_attn_indexes=encoder_global_attn_indexes,
             window_size=14,
             out_chans=prompt_embed_dim,
+            transformer_block=transformer_block
         ),
         prompt_encoder=PromptEncoder(
             embed_dim=prompt_embed_dim,
@@ -100,8 +103,66 @@ def _build_sam(
         pixel_std=[58.395, 57.12, 57.375],
     )
     sam.eval()
+    # if checkpoint is not None:
+    #     with open(checkpoint, "rb") as f:
+    #         state_dict = torch.load(f)
+    #     sam.load_state_dict(state_dict)
     if checkpoint is not None:
         with open(checkpoint, "rb") as f:
-            state_dict = torch.load(f)
-        sam.load_state_dict(state_dict)
+            state_dict = torch.load(f, map_location="cpu")
+        try:
+            if 'model' in state_dict.keys():
+                sam.load_state_dict(state_dict['model'], False)
+            else:
+                if image_size==1024 and transformer_block != None:
+                    sam.load_state_dict(state_dict, False)
+                else:
+                    sam.load_state_dict(state_dict)
+        except:
+            print('*******interpolate')
+            new_state_dict = load_from(sam, state_dict, image_size, vit_patch_size)   
+            sam.load_state_dict(new_state_dict)
+        print(f"*******load {checkpoint}")
     return sam
+
+
+
+def load_from(sam, state_dicts, image_size, vit_patch_size):
+
+    sam_dict = sam.state_dict()
+    except_keys = ['mask_tokens', 'output_hypernetworks_mlps', 'iou_prediction_head']
+    new_state_dict = {k: v for k, v in state_dicts.items() if
+                      k in sam_dict.keys() and except_keys[0] not in k and except_keys[1] not in k and except_keys[2] not in k}
+    pos_embed = new_state_dict['image_encoder.pos_embed']
+    token_size = int(image_size // vit_patch_size)
+    if pos_embed.shape[1] != token_size:
+        # resize pos embedding, which may sacrifice the performance, but I have no better idea
+        pos_embed = pos_embed.permute(0, 3, 1, 2)  # [b, c, h, w]
+        pos_embed = F.interpolate(pos_embed, (token_size, token_size), mode='bilinear', align_corners=False)
+        pos_embed = pos_embed.permute(0, 2, 3, 1)  # [b, h, w, c]
+        new_state_dict['image_encoder.pos_embed'] = pos_embed
+        rel_pos_keys = [k for k in sam_dict.keys() if 'rel_pos' in k]
+
+        global_rel_pos_keys = [k for k in rel_pos_keys if 
+                                                        '2' in k or 
+                                                        '5' in k or 
+                                                        '7' in k or 
+                                                        '8' in k or 
+                                                        '11' in k or 
+                                                        '13' in k or
+                                                        '15' in k or 
+                                                        '23' in k or 
+                                                        '31' in k] 
+        # print(sam_dict)
+        for k in global_rel_pos_keys:
+            h_check, w_check = sam_dict[k].shape
+            rel_pos_params = new_state_dict[k]
+            h, w = rel_pos_params.shape
+            rel_pos_params = rel_pos_params.unsqueeze(0).unsqueeze(0)
+            if h != h_check or w != w_check:
+                rel_pos_params = F.interpolate(rel_pos_params, (h_check, w_check), mode='bilinear', align_corners=False)
+
+            new_state_dict[k] = rel_pos_params[0, 0, ...]
+
+    sam_dict.update(new_state_dict)
+    return sam_dict
